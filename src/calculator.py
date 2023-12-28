0c94f426b58dd593eb21e7af101eea71ab69ed93
Derek Lee <dereklee0310@gmail.com>
2023-12-29 04:11:47 +0800

    WIP: Add time limit spin fishing and update script
    - Add an spin fishing profile that will stop at 7 A.M.
    
    - Improve calculator.py with header, docstring, and better UI
    
    - Update requirements.txt for upcoming playsound package

--- src/calculator.py
+++ src/calculator.py
@@ -1,7 +1,31 @@
-friction_brake_wear = float(input('Friction Brake Wear:'))
-max_drag = 7.7 * (100 - friction_brake_wear)
+"""
+Calculate the maximum friction brake you can use without breaking the tackle.
 
-leader_wear = float(input('Leader Wear:'))
-max_leader_load = 6.4 * (100 - leader_wear)
+Usage: calculator.py 
 
-print('Max friction brake: ', min(max_leader_load * 30 / max_drag, 29))
+Todo: use argv as input
+"""
+
+def get_reel_TMD():
+    """
+    Calculate the reel's true max drag based on user input.
+
+    :returns: reel's true max drag
+    """
+    max_drag = float(input("Reel's max drag (kg): "))
+    friction_brake_wear = float(input("Reel's friction brake wear (%): "))
+    return max_drag * (100 - friction_brake_wear)
+
+def get_leader_TLC():
+    """
+    Calculate the leader's true load capacity based on user input.
+
+    :returns: leader's true load capacity
+    """ 
+    leader_load_capacity = float(input("Leader's load capacity (kg): "))
+    leader_wear = float(input("Leader's wear (%): "))
+    return leader_load_capacity * (100 - leader_wear)
+
+if __name__ == '__main__':
+    tmd, tlc = get_reel_TMD(), get_leader_TLC()
+    print(f'Max friction brake: {int(min(tlc * 30 / tmd, 29))}')
