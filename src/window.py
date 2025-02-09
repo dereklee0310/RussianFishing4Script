e5039c0440871742fa828384d72d3b6965246376
Derek Lee <dereklee0310@gmail.com>
2025-02-10 00:24:58 +0800

    Refactor window.py
    - Migrate to CfgNode
    
    - Replace get_coord_bases() and get_window_size() with get_box()
    
    - Add size validation

---
+++ src/window.py
@@ -0,0 +1,112 @@
+"""
+Module for window controller.
+"""
+
+import logging
+import sys
+from time import sleep
+
+# import win32api, win32con
+import pyautogui as pag
+import win32con
+import win32gui
+
+logger = logging.getLogger(__name__)
+
+
+class Window:
+    """Controller for terminal and game windows management."""
+
+    def __init__(self, game_window_title="Russian Fishing 4"):
+        """Constructor method.
+
+        :param title: game title, defaults to 'Russian Fishing 4'
+        :type title: str, optional
+        """
+        self._title = game_window_title
+        self._script_hwnd = self._get_cur_hwnd()
+        self._game_hwnd = self._get_game_hwnd()
+
+    def _get_cur_hwnd(self) -> int:
+        """Get the handle of the terminal.
+
+        :return: process handle
+        :rtype: int
+        """
+        return win32gui.GetForegroundWindow()
+
+    def _get_game_hwnd(self) -> int:
+        """Get the handle of the game.
+
+        :return: process handle
+        :rtype: int
+        """
+        hwnd = win32gui.FindWindow(None, self._title)
+        # print(win32gui.GetClassName(hwnd)) # class name: UnityWndClass
+        if not hwnd:
+            logger.error("Failed to locate the game window: %s", self._title)
+            sys.exit()
+        return hwnd
+
+    def is_title_bar_exist(self) -> bool:
+        """Check if the game window is in windowed mode.
+
+        :return: True if yes, False otherwise
+        :rtype: bool
+        """
+        style = win32gui.GetWindowLong(self._game_hwnd, win32con.GWL_STYLE)
+        return style & win32con.WS_CAPTION
+
+    def get_box(self):
+        base_x, base_y, _, _ = win32gui.GetWindowRect(self._game_hwnd)
+        if self.is_title_bar_exist():
+            base_x += 8
+            base_y += 31
+        left, top, right, bottom = win32gui.GetClientRect(self._game_hwnd)
+        width = right - left
+        height = bottom - top
+        return base_x, base_y, width, height
+
+    def activate_script_window(self) -> None:
+        """Focus terminal."""
+        pag.press("alt")
+        win32gui.SetForegroundWindow(
+            self._script_hwnd
+        )  # fullscreen mode is not supported
+        sleep(0.25)
+
+    def activate_game_window(self) -> None:
+        """Focus game window."""
+        pag.press("alt")
+        win32gui.SetForegroundWindow(
+            self._game_hwnd
+        )  # fullscreen mode is not supported
+        sleep(0.25)
+
+
+    def is_size_valid(self) -> bool:
+        if self.is_title_bar_exist():
+            # logger.error("Invalid display mode: window mode")
+            logger.info("Window mode detected, if you want to move the game window, " +
+                        "please restart the script after moving it.")
+        width, height = self.get_box()[2:]
+        if (width, height) in ((2560, 1440), (1920, 1080), (1600, 900)):
+            return True
+
+        logger.warning(
+            "Window size %s not supported, must be 2560x1440, 1920x1080 or 1600x900",
+            f"{width}x{height}",
+        )
+        logger.error('Window mode must be "Borderless windowed" or "Window mode"')
+        logger.info("Snag detection and friction brake changing will be disabled")
+        return False
+
+
+if __name__ == "__main__":
+    w = Window("Russian Fishing 4")
+    w.activate_game_window()
+    print(w.get_window_size())
+    print(w.get_coord_bases())
+
+# SetForegroundWindow bug reference :
+# https://stackoverflow.com/questions/56857560/win32gui-setforegroundwindowhandle-not-working-in-loop
