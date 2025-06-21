206d6d33bcb38a74ced14dcd10df525738d7b694
Derek Lee <dereklee0310@gmail.com>
2025-06-21 21:29:44 +0800

    Update compile script

--- dev-tools/compile.bat
+++ dev-tools/compile.bat
@@ -1,2 +1,5 @@
-python -m nuitka tools\main.py --windows-icon-from-ico=./dev-tools/main.ico --output-dir=dist --standalone --onefile --mingw64 --include-data-dir=static=static
+python -m nuitka tools\main.py --windows-icon-from-ico=./static/readme/main.ico --output-dir=dist --standalone --onefile --mingw64 --include-data-dir=static=static
+if not exist dist mkdir dist
+if not exist dist\screenshots mkdir dist\screenshots
+if not exist dist\logs mkdir dist\logs
 tar.exe -a -c -f dist\rf4s.zip LICENSE config.yaml -C dist main.exe screenshots logs
